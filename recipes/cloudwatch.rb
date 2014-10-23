c2c35684095e7d90d0b09ad449b6ee6bce71dca8
cbbarclay <cbbarclay@hotmail.com>
2014-10-23 14:02:49 -0700

    Create cloudwatch.rb

---
+++ recipes/cloudwatch.rb
@@ -0,0 +1,18 @@
+powershell_script 'Install IIS' do
+  code <<-EOH
+    $EC2SettingsFile="C:\Program Files\Amazon\Ec2ConfigService\Settings\Config.xml"
+    $xml = [xml](get-content $EC2SettingsFile)
+    $xmlElement = $xml.get_DocumentElement()
+    $xmlElementToModify = $xmlElement.Plugins
+
+    foreach ($element in $xmlElementToModify.Plugin)
+    {
+        if ($element.name -eq "AWS.EC2.Windows.CloudWatch.PlugIn")
+        {
+            $element.State="Enabled"
+        }
+    }
+    $xml.Save($EC2SettingsFile)
+  EOH
+
+end
