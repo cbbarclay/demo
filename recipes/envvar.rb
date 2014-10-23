b41b07d2a2f9b8d4d23754e25aec3cfeab498fab
cbbarclay <cbbarclay@hotmail.com>
2014-10-23 16:53:27 -0700

    Create envvar.rb

---
+++ recipes/envvar.rb
@@ -0,0 +1,10 @@
+# pass an env var to script
+powershell "read-env-var" do
+  cwd Chef::Config[:file_cache_path]
+  environment ({'foo' => 'BAZ'})
+  code <<-EOH
+  $stream = [System.IO.StreamWriter] "./test-read-env-var.txt"
+  $stream.WriteLine("FOO is $env:foo")
+  $stream.close()
+  EOH
+end
